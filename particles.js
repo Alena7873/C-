758ddcd613a79e10b83ac7dec939e8d6b65243aa
Alena <115954741+Alena7873@users.noreply.github.com>
2023-02-18 00:52:23 +0200

    7 блок початок

---
+++ particles.js
@@ -0,0 +1,7 @@
+function myFunction1() {
+    document.getElementById("textArt").innerHTML = "Cleaning";
+}
+
+function myFunction() {
+    document.getElementById("paragraphSeven").innerHTML = "Qualified workers, professional equipment, specialized detergents, knowledge of the characteristics of various materials - these factors ensure high-quality cleaning in a short period of time.";
+}
